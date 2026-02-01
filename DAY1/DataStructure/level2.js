2d037dcaaea84916e3cd0f6ace96825dedf0b745
syed abdul qahar <syedabdulqahar2004@gmail.com>
2026-02-01 22:00:25 +0500

    Level 2 added

--- DAY1/DataStructure/level2.js
+++ DAY1/DataStructure/level2.js
@@ -21,9 +21,9 @@ console.log(`2. Given a number, reverse it using a loop.
           Example: 123 → 321`);
 console.log("3. Check Whether a giver number is prime or not");
 console.log(`4. Find the largest number from these values :  10,45,23,99,2`);
-console.log(`5. Count how many digits are in a number`);
+console.log(`5. Find the largest number from dynamic array ?`);
+console.log(`6. Count how many digits are in a number ? `);
 
-const { count } = require("console");
 const { stdin, stdout } = require("process");
 const readline = require("readline");
 const r1 = readline.createInterface({
@@ -48,19 +48,70 @@ function CheckDivisible() {
   console.log("-------------------------------");
 }
 function ReverseNumber() {
-  console.log("--------------  Reversed Number  --------------------");
-  r1.question("Enter A Number to reverse it : ", (value) => {
-    valuetoReverse = Number(value);
-    let reversedNumber = 0;
-    if (valuetoReverse >= 0 && valuetoReverse <= 9) {
-      console.log(`Reversed Number : ${valuetoReverse}`);
+  r1.question("Enter A Number to Reverse it : ", (value) => {
+    valuetoreverse = Number(value);
+    if (valuetoreverse >= 0 && valuetoreverse <= 9) {
+      console.log(`Reversed value : ${valuetoreverse} `);
+      return;
     }
-    while (valuetoReverse !== 0) {
-      reversedNumber = reversedNumber * 10 + (valuetoReverse % 10); //0+3
-      valuetoReverse = Math.floor(valuetoReverse / 10);
+    let reversedValue = 0;
+    while (valuetoreverse != 0) {
+      reversedValue = reversedValue * 10 + (valuetoreverse % 10); //123 123%10=3 |
+      valuetoreverse = Math.floor(valuetoreverse / 10);
     }
-    console.log(`Reversed Number : ${reversedNumber}`);
+    console.log(`Reversed Value : ${reversedValue}`);
   });
+}
+function PrimeCheck() {
+  r1.question("Enter a Number to check if it is prime or not : ", (value) => {
+    Number = Number(value);
+    if (Number % 2 == 0) {
+      console.log(`Number ${Number} is Prime `);
+    }
+    console.log(`${Number} is Not Prime`);
+  });
+}
+function LargestValue() {
+  let arr = [10, 45, 60, 20, 10];
+  console.log(`Array to check : ${arr}`);
+  let largest = arr[0];
+  for (let i = 1; i <= arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  console.log(`${largest} is the highest`);
+}
+function dynamicLargestValue() {
+  r1.question(
+    "Enter value in form of string to check which is largest element ? : ",
+    (value) => {
+      let array = value.split(",");
+      let FinalArrray = [];
+      for (let i = 0; i < array.length; i++) {
+        FinalArrray.push(Number(array[i]));
+      }
+      let largestValue = 0;
+      for (let i = 0; i < FinalArrray.length; i++) {
+        if (FinalArrray[i] > largestValue) {
+          largestValue = FinalArrray[i];
+        }
+      }
+      console.log(
+        `Largest Value from the array you entered is : ${largestValue}`,
+      );
+    },
+  );
+}
+function CountNumber() {
+  r1.question(
+    "Enter the Number to check its size or count of integers used in it ?",
+    (value) => {
+      let inputNumber = value;
+      let count = Number(inputNumber.length);
+      console.log(`Number of Digits Entered by user is : ${count}`);
+    },
+  );
 }
 r1.question("Enter Your Choice : ", (choice) => {
   choice = Number(choice);
@@ -73,13 +124,16 @@ r1.question("Enter Your Choice : ", (choice) => {
       ReverseNumber();
       break;
     case 3:
-      console.log("3 wala chala");
+      PrimeCheck();
       break;
     case 4:
-      console.log("4 wala chala");
+      LargestValue();
       break;
     case 5:
-      console.log("5 wala chala");
+      dynamicLargestValue();
+      break;
+    case 6:
+      CountNumber();
       break;
 
     default:
