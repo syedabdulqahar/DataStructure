7da909e99c5acfdee7f399a01c2295d1b4c63856
syed abdul qahar <syedabdulqahar2004@gmail.com>
2026-02-05 11:26:11 +0500

    level 3 of day 1 is done

--- DAY1/DataStructure/level3.js
+++ DAY1/DataStructure/level3.js
@@ -37,12 +37,13 @@ function case1() {
       );
       for (i = minRange; i <= maxRange; i++) {
         if (i % 2 != 0) {
-          console.log(`Number ${i} is ddd`);
+          console.log(`Number ${i} is odd`);
         }
       }
+      console.log("\n");
+      display();
     },
   );
-  display();
 }
 function case2() {
   r1.question("Enter a value to check its factorial : ", (value) => {
@@ -52,17 +53,84 @@ function case2() {
       factorial1 = factorial1 * value;
       value--;
     }
-    console.log(`Factorial of Number ${value1} is ${factorial1} `);
+    console.log(`\t Factorial of Number ${value1} is ${factorial1} `);
+    console.log("\n");
+    display();
   });
-  display();
 }
-function case3() {}
+function case3() {
+  let Num;
+  r1.question(
+    "Enter a Number to check whether if it is palindrome ? : ", //string input
+    (value) => {
+      Num = Number(value);
+      console.log(`Original Number : ${Num}`);
+      let original = Num;
+      let reversed = 0; //initializing is important
+      while (Num !== 0) {
+        reversed = reversed * 10 + (Num % 10);
+        Num = Math.floor(Num / 10);
+      }
+      console.log(`Reversed Number : ${reversed}`);
+      if (original === reversed) {
+        console.log(
+          `\t Number is palindrom  | < ${original} >  ===  < ${reversed} > `,
+        );
+      } else {
+        console.log(
+          `\t Number is NOT palindrom  | < ${original} >  ===  < ${reversed} > `,
+        );
+      }
+      console.log("\n");
+      display();
+    },
+  );
+}
+function case4() {
+  r1.question(
+    "Enter N as a Max Range to calculate of number between 1 to n  :  ",
+    (value) => {
+      let num = Number(value);
+      let sum = 0;
+      for (let i = 1; i <= num; i++) {
+        if (i % 2 === 0) {
+          sum = sum + i;
+        }
+      }
+      console.log(` \t Sum of Even Number between 1 to ${num} = ${sum}`);
+      console.log("\n");
+      display();
+    },
+  );
+}
+function case5() {
+  let Num = 0;
+  r1.question("Enter a number to check its fabiconni : ", (value) => {
+    Num = Number(value);
+    let a = 0;
+    let b = 1;
+
+    for (let i = 0; i < Num; i++) {
+      console.log(a);
+      let next = a + b;
+      a = b;
+      b = next;
+    }
+    console.log("\n");
+    display();
+  });
+}
 
 function display() {
   console.log("What do you want (Just press Numbers) ?");
   console.log("1. To Print Odd Number between Dynamic Ranges ");
   console.log("2. To Check Factorial of the Number ");
   console.log("3. To check Palindrome ");
+  console.log(
+    "4. To calculate Sum of all EVEN elements  (1 to n) N will be Entered by User",
+  );
+  console.log("5. To check Fabiconni of a user input range ");
+
   let choice;
   r1.question("Enter Your Choice : ", (value) => {
     choice = Number(value);
@@ -75,6 +143,12 @@ function display() {
         break;
       case 3:
         case3();
+        break;
+      case 4:
+        case4();
+        break;
+      case 5:
+        case5();
         break;
       default:
         console.log(`You Entered Wrong Number`);
