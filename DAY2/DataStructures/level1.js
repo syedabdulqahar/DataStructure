9ad64e48468c1850c856ee87cab1dd6779edbd42
syed abdul qahar <syedabdulqahar2004@gmail.com>
2026-02-07 12:59:12 +0500

    Arrays Level1 done

--- DAY2/DataStructures/level1.js
+++ DAY2/DataStructures/level1.js
@@ -27,6 +27,7 @@ const r1 = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+let array1 = []; //initialized Global Array
 function PrintElements() {
   //Entering Elements in array from user
   //store it in number in arrray
@@ -36,29 +37,94 @@ function PrintElements() {
     (value) => {
       value = value.split(",");
       value = value.map(Number);
-      let array1 = [];
 
       for (let i = 0; i < value.length; i++) {
         array1.push(value[i]);
       }
-      console.log("Whole array : ");
-      console.log(array1);
-      console.log("Printing Elments :");
+      console.log("\t Whole array : ");
+      console.log(`\t ${array1}`);
+      console.log("\t Printing Elements :");
       for (let i = 0; i < array1.length; i++) {
-        console.log(`${i} = ${array1[i]}`);
+        console.log(`\t    array1[${i}] = ${array1[i]}`);
       }
+      console.log("\n");
+
+      display();
     },
   );
 }
+function CountElements() {
+  let count = 0;
+  for (let i = 0; i < array1.length; i++) {
+    console.log(`\t Element ${i} of array1 was ${array1[i]} `);
+    count++;
+  }
+  console.log(`\t Count of Array1 is : ${count}`);
+  console.log("\n");
+  display();
+}
+function SumofArray() {
+  if (array1.length === 0) {
+    console.log(`\n \t X please input array by pressing 1 from the following `);
+    console.log("\n");
+    display();
+  } else {
+    let sum = 0;
+    for (let i = 0; i < array1.length; i++) {
+      sum = sum + array1[i];
+    }
+    console.log(`\t Sum of array1= [${array1}] is =  ${sum} \n`);
+    display();
+  }
+}
 
+function EvenNumber() {
+  console.log(
+    `Checking Even Number from array1[${array1.length}] = [${array1}] .... `,
+  );
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] % 2 == 0) {
+      console.log(`Number [${array1[i]}] is Even at array1[${i}]`);
+    }
+  }
+  console.log("\n");
+  display();
+}
+function LargestNumber() {
+  if (array1.length === 0) {
+    console.log(
+      `\n \t X Array is Empty ! Please enter array elements by pressing choice 1 `,
+    );
+    console.log("\n");
+    display();
+  } else {
+    let largest = 0;
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] > largest) {
+        largest = array1[i];
+      }
+    }
+    console.log(`Largest Value from array1[${array1}] is ${largest}`);
+    console.log("\n");
+    display();
+  }
+}
 function display() {
-  console.log("--------  Arrays -----");
-  console.log("1. Print elements of Array");
+  console.log(
+    "-------------------------------- Arrays ---------------------------------------",
+  );
+  console.log(
+    "= Note : Each of the following is using same array which is taken as input from 1 =",
+  );
+  console.log(
+    "-------------------------------------------------------------------------------",
+  );
+  console.log("1. Print elements of Array \t Note: 2 is dependent on 1");
   console.log("2. Count elements in Array");
   console.log("3. Sum of Array");
   console.log("4. Even Numbers in array ");
   console.log("5. Largest Number in array ");
-  r1.question("Enter Your Choice ", (value) => {
+  r1.question("- Enter Your Choice : ", (value) => {
     let choice = Number(value);
     switch (choice) {
       case 1:
