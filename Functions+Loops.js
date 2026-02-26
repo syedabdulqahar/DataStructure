483932340256c8317e01dcfbabbbd4210fd70c23
syed abdul qahar <syedabdulqahar2004@gmail.com>
2026-02-26 15:15:20 +0500

    diamond creation using loops

--- Functions+Loops.js
+++ Functions+Loops.js
@@ -1,23 +1,39 @@
-for (let i = 0; i < 5; i++) {
-  let str = "";
-  for (let k = 0; k < 5 - i; k++) {
-    str = str + " ";
+const readline = require("readline");
+const r1 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+r1.question("Enter size of diamond you want to have : ", (value) => {
+  let size = Number(value);
+  //First half of diamond
+  for (let i = 0; i < size; i++) {
+    let str = "";
+    //spaces loop
+    for (let j = 0; j < size - i; j++) {
+      str += "-";
+    }
+    for (let k = 0; k < 2 * i + 1; k++) {
+      str += "*";
+    }
+    for (let l = 0; l < size - i; l++) {
+      str += "-";
+    }
+    console.log(str);
   }
-  for (let j = 0; j < 2 * i + 1; j++) {
-    str = str + "+";
+  //second half of diamond
+  for (let i = 0; i < size; i++) {
+    let str = "";
+    //spaces loop
+    for (let j = 0; j <= i; j++) {
+      str += "-";
+    }
+    //reversed pyramid
+    for (let k = 0; k < 2 * (size - i) - 1; k++) {
+      str += "*";
+    }
+    for (let l = 0; l <= i; l++) {
+      str += "-";
+    }
+    console.log(str);
   }
-  console.log(`${str}`);
-}
-for (let i = 2; i >= 0; i--) {
-  let str = "";
-
-  for (let k = 0; k < 5 - i; k++) {
-    str += " ";
-  }
-
-  for (let j = 0; j < 2 * i + 1; j++) {
-    str += "+";
-  }
-
-  console.log(str);
-}
+});
